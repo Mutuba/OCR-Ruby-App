334d8af4505bd805230ee95df03ee9d78680a86b
Mutuba <danielmutubait@gmail.com>
2024-07-08 21:29:52 +0300

    Add tests

---
+++ spec/models/document_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Document, type: :model do
+  subject { Document.new(title: "Test Document")}
+  it 'is valid' do
+    expect(subject).to be_valid  
+  end
+
+  it 'invalid without title' do
+    subject.title= nil
+    expect(subject).not_to be_valid  
+  end
+end
