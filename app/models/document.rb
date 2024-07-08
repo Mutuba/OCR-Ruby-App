455deeabaf715b0a66546da048cc065b319bd452
Mutuba <danielmutubait@gmail.com>
2024-07-08 22:19:55 +0300

    Add controller for documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,5 @@
 class Document < ApplicationRecord
   validates :title, presence: true
+  
+  has_one_attached :file
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -10,4 +10,7 @@ RSpec.describe Document, type: :model do
     subject.title= nil
     expect(subject).not_to be_valid  
   end
+
+  #Associations
+  # it { should have_one_attached(:file) }
 end
